f82d4653388384899d4165b4fbd70cd016a9ec45
unknown <shivarajan573@gmail.com>
2025-02-11 21:21:25 +0530

    final commit

---
+++ src/test/java/dataDriven/DataDrivenTest.java
@@ -0,0 +1,61 @@
+package dataDriven;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.RichTextString;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbookFactory;
+
+public class DataDrivenTest {
+
+//	"C:\Users\Shivaraj\Desktop\testdata.xlsx"
+
+//Identify testcase column by scanning the entire 1st row
+//	one column is identified then scan the entire testcase column to identify the cart testcase row
+//	after u grab the cart row= pull all the data of that row and feed into test
+
+	public static void main(String[] args) throws IOException {
+		FileInputStream fis = new FileInputStream("C:\\Users\\Shivaraj\\Desktop\\testdata.xlsx");
+		XSSFWorkbookFactory workbook = new XSSFWorkbookFactory();
+		XSSFWorkbook book = workbook.create(fis);
+		int sheets = book.getNumberOfSheets();
+
+		for (int i = 0; i < sheets; i++) {
+			if (book.getSheetName(i).equalsIgnoreCase("testdata")) {
+				XSSFSheet sheet = book.getSheetAt(i);
+
+				// Identify testcase column by scanning the entire 1st row
+				Iterator<Row> row = sheet.rowIterator();
+				Row firstrow = row.next();
+				Iterator<Cell> cell = firstrow.cellIterator();
+				int k = 0;
+				int column = 0;
+				while (cell.hasNext()) {
+					Cell value = cell.next();
+					if (value.getStringCellValue().equalsIgnoreCase("testcase")) {
+						column = k;
+					}
+					k++;
+				}
+				System.out.println(column);
+//				one column is identified then scan the entire testcase column to identify the cart testcase row
+				while (row.hasNext()) {
+					Row r = row.next();
+					if (r.getCell(column).getStringCellValue().equalsIgnoreCase("cart")) {
+						Iterator<Cell> cellvalue = r.cellIterator();
+						while (cellvalue.hasNext()) {
+							RichTextString cv = cellvalue.next().getRichStringCellValue();
+							System.out.println(cv);
+						}
+					}
+				}
+
+			}
+		}
+	}
+}
---
+++ src/test/java/factory/BaseClass.java
@@ -0,0 +1,122 @@
+package factory;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class BaseClass {
+	static WebDriver driver;
+	static Properties p;
+	static Logger logger;
+
+	public static WebDriver initializeBrowser() throws IOException {
+		p = getProperties();
+		String executionEnv = p.getProperty(""); // enter execution env
+		String browser = p.getProperty("").toLowerCase(); // enter the browser details
+		String os = p.getProperty("os").toLowerCase();
+
+		if (executionEnv.equalsIgnoreCase("remote")) {
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+
+			// os
+			switch (os) {
+			case "windows":
+				capabilities.setPlatform(Platform.WINDOWS);
+				break;
+			case "mac":
+				capabilities.setPlatform(Platform.MAC);
+				break;
+			case "linux":
+				capabilities.setPlatform(Platform.LINUX);
+				break;
+			default:
+				System.out.println("No Matching OS");
+				return null;
+			}
+
+			// browser
+			switch (browser) {
+			case "chrome":
+				capabilities.setBrowserName("chrome");
+				break;
+			case "edge":
+				capabilities.setBrowserName("MicrosoftEdge");
+				break;
+			case "firefox":
+				capabilities.setBrowserName("Firefox");
+				break;
+			default:
+				System.out.println("No Matching Browser");
+				return driver;
+			}
+
+			driver = new RemoteWebDriver(new URL(""), capabilities);
+		} else if (executionEnv.equals("local")) {
+			switch (browser.toLowerCase()) {
+			case "chrome":
+				driver = new ChromeDriver();
+				break;
+			case "edge":
+				driver = new EdgeDriver();
+				break;
+			case "firefox":
+				driver = new FirefoxDriver();
+				break;
+			default:
+				System.out.println("No Matching Browser");
+				driver = null;
+			}
+		}
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+		return driver;
+	}
+
+	public static WebDriver getDriver() {
+		return driver;
+	}
+
+	public static Properties getProperties() throws IOException {
+		FileReader file = new FileReader(System.getProperty("user.dir") + "\\src\\test\\resources\\config.properties");
+		p = new Properties();
+		p.load(file);
+		return p;
+	}
+
+	public static Logger getLogger() {
+		logger = LogManager.getLogger();
+		return logger;
+	}
+
+	public static String randomString() {
+		String generatedString = RandomStringUtils.randomAlphanumeric(5);
+		return generatedString;
+	}
+
+	public static String randomNumber() {
+		String geberatedString = RandomStringUtils.randomNumeric(10);
+		return geberatedString;
+	}
+
+	public static String randomAlphanumeric() {
+		String str = RandomStringUtils.randomAlphanumeric(5);
+		String num = RandomStringUtils.randomNumeric(10);
+		return str + num;
+	}
+}
